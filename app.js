c8171be130edd753c3885da3a48421d9933b4d4b
hupanpan <hupanpan@jingdata.com>
2019-11-08 11:55:54 +0800

    合并

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  // 1.0分支第二次提交
 
     // 登录
     wx.login({
